ece466454c69c864abaa5fa4e419b39d9dc5d7bc
zhengqigo <zhengqigo@vip.163.com>
2019-01-25 15:04:26 +0800

    nacos ok version

--- src/main/java/cn/fuelteam/controller/DubboController.java
+++ src/main/java/cn/fuelteam/controller/DubboController.java
@@ -1,24 +1,19 @@
 package cn.fuelteam.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
-
 @RestController
+@RefreshScope
 public class DubboController {
 
-    @NacosValue(value = "${name:unknown}", autoRefreshed = true)
+    @Value(value = "${name:unknown}")
     private String name;
 
     @RequestMapping(value = "/hello")
     public String dubboSayHello() {
         return "hello " + name;
-    }
-
-    @NacosConfigListener(dataId = "DEMO_WEB", groupId = "DEV_GROUP", timeout = 500)
-    public void onChange(String newContent) throws Exception {
-        System.out.println("onChange : " + newContent);
     }
 }
