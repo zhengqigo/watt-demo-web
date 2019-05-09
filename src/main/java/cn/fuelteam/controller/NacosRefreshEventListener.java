cabe8c56e8710a9831bbbd1a58e84316c1b00a65
zhengqigo <zhengqigo@vip.163.com>
2019-05-09 10:44:45 +0800

    tijiao

--- src/main/java/cn/fuelteam/controller/NacosRefreshEventListener.java
+++ src/main/java/cn/fuelteam/controller/NacosRefreshEventListener.java
@@ -3,17 +3,26 @@ package cn.fuelteam.controller;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.cloud.endpoint.event.RefreshEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class NacosRefreshEventListener {
 
+    private Logger logger = LoggerFactory.getLogger(NacosRefreshEventListener.class);
+
+    @Autowired
+    private Environment environment;
+
     private ContextRefresher refresh;
-    
+
     private AtomicBoolean ready = new AtomicBoolean(false);
 
     public NacosRefreshEventListener(ContextRefresher refresh) {
@@ -27,10 +36,10 @@ public class NacosRefreshEventListener {
 
     @EventListener
     public void handle(RefreshEvent refreshEvent) {
-        if (this.ready.get()) {
-            System.out.println("RefreshEvent received " + refreshEvent.getEventDesc());
-            Set<String> keys = this.refresh.refresh();
-            System.out.println("RefreshEvent keys changed: " + keys);
+        if (!this.ready.get()) return;
+        Set<String> keys = this.refresh.refresh();
+        for (String key : keys) {
+            logger.info("Key {} refreshed value {}", key, environment.getProperty(key));
         }
     }
 }
